9a1aa75d1fa015f99c9b6bdb5b2a4bade514627b
Ibtesam <ibtesamgr8@gmail.com>
2016-02-18 20:06:04 +0500

    categories model created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160217172557) do
+ActiveRecord::Schema.define(version: 20160218150150) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.string   "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
